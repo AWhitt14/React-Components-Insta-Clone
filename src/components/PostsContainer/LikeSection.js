17108a62677c65aabf7c69adf271734e9be00219
Alex Whitt <whitta14@gmail.com>
2020-06-20 09:38:27 -0400

    finished all

--- src/components/PostsContainer/LikeSection.js
+++ src/components/PostsContainer/LikeSection.js
@@ -6,20 +6,21 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faComment, faHeart } from '@fortawesome/free-regular-svg-icons'
 
 const LikeSection = props => {
+
   return (
     <div>
       <div
         className="like-section"
         key="likes-icons-container"
       >
-        <div className="like-section-wrapper">
-          <FontAwesomeIcon icon={faHeart} />
+        <div className="like-section-wrapper" >
+          <FontAwesomeIcon icon={faHeart} onClick={props.incrementlikes}/>
         </div>
         <div className="like-section-wrapper">
           <FontAwesomeIcon icon={faComment} />
         </div>
       </div>
-      <p className="like-number">27 likes</p>
+      <p className="like-number">{props.likes}</p>
     </div>
   )
 };
